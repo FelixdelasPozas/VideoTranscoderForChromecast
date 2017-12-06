c3123a3bf34d480a8f43bf586ac07b7c6628469d
Felix de las Pozas Alvarez <felixdelaspozas@gmail.com>
2017-12-06 21:41:55 +0100

    Utils for video transcoder.

---
+++ Utils.cpp
@@ -0,0 +1,247 @@
+/*
+ File: Utils.cpp
+ Created on: 17/4/2015
+ Author: Felix de las Pozas Alvarez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Project
+#include "Utils.h"
+
+// Qt
+#include <QSettings>
+#include <QDirIterator>
+#include <QTemporaryFile>
+
+// C++
+#include <thread>
+
+const QStringList Utils::MOVIE_FILE_EXTENSIONS   = {"*.mp4", "*.avi", "*.ogv", "*.webm", "*.mkv", "*.mpg", "*.mpeg" };
+const QString     Utils::TEMPORAL_FILE_EXTENSION = QString(".VideoTranscoderTemporal");
+
+const QString Utils::TranscoderConfiguration::ROOT_DIRECTORY    = QObject::tr("Root directory");
+const QString Utils::TranscoderConfiguration::NUMBER_OF_THREADS = QObject::tr("Number of threads");
+const QString Utils::TranscoderConfiguration::VIDEO_CODEC       = QObject::tr("Video codec");
+const QString Utils::TranscoderConfiguration::VIDEO_BITRATE     = QObject::tr("Video bitrate");
+const QString Utils::TranscoderConfiguration::AUDIO_CODEC       = QObject::tr("Audio codec");
+const QString Utils::TranscoderConfiguration::AUDIO_BITRATE     = QObject::tr("Audio bitrate");
+
+
+//-----------------------------------------------------------------
+bool Utils::isVideoFile(const QFileInfo &file)
+{
+  auto extension = file.absoluteFilePath().split('.').last().toLower();
+
+  return MOVIE_FILE_EXTENSIONS.contains("*." + extension);
+}
+
+//-----------------------------------------------------------------
+QList<QFileInfo> Utils::findFiles(const QDir initialDir,
+                                  const QStringList extensions,
+                                  bool with_subdirectories,
+                                  const std::function<bool (const QFileInfo &)> &condition)
+{
+  QList<QFileInfo> otherFilesFound, videoFilesFound;
+
+  auto startingDir = initialDir;
+  startingDir.setFilter(QDir::Files | QDir::Dirs | QDir::NoDot | QDir::NoDotDot);
+  startingDir.setNameFilters(extensions);
+
+  auto flag = (with_subdirectories ? QDirIterator::Subdirectories : QDirIterator::NoIteratorFlags);
+  QDirIterator it(startingDir, flag);
+  while (it.hasNext())
+  {
+    it.next();
+
+    auto info = it.fileInfo();
+
+    if(!condition(info)) continue;
+
+    auto extension = info.absoluteFilePath().split('.').last().toLower();
+
+    if (isVideoFile(info))
+    {
+      videoFilesFound << info;
+    }
+    else
+    {
+      otherFilesFound << info;
+    }
+  }
+
+  return videoFilesFound + otherFilesFound;
+}
+
+//-----------------------------------------------------------------
+Utils::TranscoderConfiguration::TranscoderConfiguration()
+: m_number_of_threads             {0}
+, m_videoCodec                    {VideoCodec::VP8}
+, m_videoBitrate                  {1000}
+, m_audioCodec                    {AudioCodec::VORBIS}
+, m_audioBitrate                  {128}
+{
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::load()
+{
+  QSettings settings("Felix de las Pozas Alvarez", "VideoTranscoder");
+
+  m_root_directory    = settings.value(ROOT_DIRECTORY, QDir::currentPath()).toString();
+  m_number_of_threads = settings.value(NUMBER_OF_THREADS, std::thread::hardware_concurrency() /2).toInt();
+  m_videoCodec        = static_cast<VideoCodec>(settings.value(VIDEO_CODEC, 0).toInt());
+  m_videoBitrate      = settings.value(VIDEO_BITRATE, 0).toInt();
+  m_audioCodec        = static_cast<AudioCodec>(settings.value(AUDIO_CODEC, 0).toInt());
+  m_audioBitrate      = settings.value(AUDIO_BITRATE, 0).toInt();
+
+  // go to parent or home if the saved directory no longer exists.
+  m_root_directory = validDirectoryCheck(m_root_directory);
+
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::save() const
+{
+  QSettings settings("Felix de las Pozas Alvarez", "VideoTranscoder");
+
+  settings.setValue(ROOT_DIRECTORY, QDir{validDirectoryCheck(m_root_directory)}.absolutePath());
+  settings.setValue(NUMBER_OF_THREADS, m_number_of_threads);
+  settings.setValue(VIDEO_CODEC, static_cast<int>(m_videoCodec));
+  settings.setValue(VIDEO_BITRATE, m_videoBitrate);
+  settings.setValue(AUDIO_CODEC, static_cast<int>(m_audioCodec));
+  settings.setValue(AUDIO_BITRATE, m_audioBitrate);
+
+  settings.sync();
+}
+
+//-----------------------------------------------------------------
+const QString& Utils::TranscoderConfiguration::rootDirectory() const
+{
+  return m_root_directory;
+}
+
+//-----------------------------------------------------------------
+int Utils::TranscoderConfiguration::numberOfThreads() const
+{
+  return m_number_of_threads;
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::setRootDirectory(const QString& path)
+{
+  m_root_directory = path;
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::setNumberOfThreads(int value)
+{
+  m_number_of_threads = value;
+}
+
+//-----------------------------------------------------------------
+const Utils::TranscoderConfiguration::VideoCodec Utils::TranscoderConfiguration::videoCodec() const
+{
+  return m_videoCodec;
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::setVideoCodec(const VideoCodec codec)
+{
+  m_videoCodec = codec;
+}
+
+//-----------------------------------------------------------------
+const Utils::TranscoderConfiguration::AudioCodec Utils::TranscoderConfiguration::audioCodec() const
+{
+  return m_audioCodec;
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::setAudioCodec(const AudioCodec codec)
+{
+  m_audioCodec = codec;
+}
+
+//-----------------------------------------------------------------
+const int Utils::TranscoderConfiguration::videoBitrate() const
+{
+  return m_videoBitrate;
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::setVideoBitrate(const int bitrate)
+{
+  m_videoBitrate = bitrate;
+}
+
+//-----------------------------------------------------------------
+const int Utils::TranscoderConfiguration::audioBitrate() const
+{
+  return m_audioBitrate;
+}
+
+//-----------------------------------------------------------------
+void Utils::TranscoderConfiguration::setAudioBitrate(const int bitrate)
+{
+  m_audioBitrate = bitrate;
+}
+
+//-----------------------------------------------------------------
+const bool Utils::TranscoderConfiguration::isValid() const
+{
+  // TODO
+  return false;
+}
+
+//-----------------------------------------------------------------
+const QString Utils::validDirectoryCheck(const QString& directory)
+{
+  QStringList drivesPath;
+  for(auto path: QDir::drives())
+  {
+    drivesPath << path.absolutePath();
+  }
+
+  // go to parent or home if the saved directory no longer exists.
+  QDir dir{directory};
+  while(!dir.exists() && !dir.isRoot() && !drivesPath.contains(dir.absolutePath()))
+  {
+    // NOTE: dir.cdUp() doesn't work if the path is nested in more than two directories that
+    // don't exist, returning false.
+    auto path = dir.absolutePath();
+
+    bool isValidDrive = false;
+    for(auto drive: drivesPath)
+    {
+      if(path.startsWith(drive))
+      {
+        isValidDrive = true;
+        break;
+      }
+    }
+
+    if(!isValidDrive) break;
+
+    path = path.left(path.lastIndexOf('/'));
+    dir = QDir{path};
+  }
+
+  if(!dir.exists())
+  {
+    return QDir::homePath();
+  }
+
+  return QDir::toNativeSeparators(dir.absolutePath());
+}
---
+++ Utils.h
@@ -0,0 +1,181 @@
+/*
+  File: Utils.h
+  Created on: 17/4/2015
+  Author: Felix de las Pozas Alvarez
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+// C++
+#include <functional>
+
+// Qt
+#include <QDir>
+#include <QString>
+#include <QPair>
+#include <QMutex>
+
+namespace Utils
+{
+  extern const QStringList MOVIE_FILE_EXTENSIONS;
+  extern const QString TEMPORAL_FILE_EXTENSION;
+  static QMutex s_mutex;
+
+  /** \brief Returns true if the file given as parameter has a video extension.
+   * \param[in] file file QFileInfo struct.
+   *
+   */
+  bool isVideoFile(const QFileInfo &file);
+
+  /** \brief Checks the given directory for existance and readability. If the directory is
+   *  not readable or doesn't exist it will try the parent recursively until returning a valid
+   *  path or the user home directory.
+   * \param[in] directory path.
+   */
+  const QString validDirectoryCheck(const QString &directory);
+
+  /** \brief Returns the files in the specified directory tree that has the specified extensions.
+   * \param[in] rootDir starting directory.
+   * \param[in] filters extensions of the files to return.
+   * \param[in] with_subdirectories boolean that indicates if all the files in the subdiretories that
+   *            comply the conditions must be returned.
+   * \param[in] condition additional condition that the files must comply with.
+   *
+   * NOTE: while this will return any file info (that complies with the filter and the conditions) mp3
+   *       files will be returned first. That's because i want to process those before anything else.
+   *       Yes, it's arbitrary but doesn't affect the results.
+   *
+   */
+  QList<QFileInfo> findFiles(const QDir rootDirectory,
+                             const QStringList extensions,
+                             bool with_subdirectories = true,
+                             const std::function<bool (const QFileInfo &)> &condition = [](const QFileInfo &info) { return true; });
+
+  /** \class TranscoderConfiguration
+   * \brief Implements the configuration storage/management.
+   *
+   */
+  class TranscoderConfiguration
+  {
+    public:
+      enum class VideoCodec { VP8 = 0, VP9, H264, H265 }; /** video codec identifiers. */
+      enum class AudioCodec { VORBIS = 0, MP3, AAC };     /** audio codec identifiers. */
+
+      /** \brief TranscoderConfiguration class constructor.
+       *
+       */
+      TranscoderConfiguration();
+
+      /** \brief Loads the configuration data from disk.
+       *
+       */
+      void load();
+
+      /** \brief Saves the configuration to disk.
+       *
+       */
+      void save() const;
+
+      /** \brief Returns the root directory to start searching for files.
+       *
+       */
+      const QString &rootDirectory() const;
+
+      /** \brief Sets the root directory to start searching for files to transcode.
+       * \param[in] path root directory path.
+       *
+       */
+      void setRootDirectory(const QString &path);
+
+      /** \brief Returns the number of simultaneous threads in the transcoding process.
+       *
+       */
+      int numberOfThreads() const;
+
+      /** \brief Sets the number of simultaneous threads to use in the transcoding process.
+       * \param[in] value number of threads.
+       *
+       */
+      void setNumberOfThreads(int value);
+
+      /** \brief Returns the output file video codec.
+       *
+       */
+      const VideoCodec videoCodec() const;
+
+      /** \brief Sets the output file video codec.
+       * \param[in] codec Video codec identifier.
+       *
+       */
+      void setVideoCodec(const VideoCodec codec);
+
+      /** \brief Returns the output file audio codec.
+       *
+       */
+      const AudioCodec audioCodec() const;
+
+      /** \brief Sets the output file audio codec.
+       * \param[in] codec Audio codec identifier.
+       *
+       */
+      void setAudioCodec(const AudioCodec codec);
+
+      /** \brief Returns the output file video bitrate.
+       *
+       */
+      const int videoBitrate() const;
+
+      /** \brief Sets the output file video bitrate.
+       * \param[in] bitrate Integer value.
+       *
+       */
+      void setVideoBitrate(const int bitrate);
+
+      /** \brief Returns the output file audio bitrate.
+       *
+       */
+      const int audioBitrate() const;
+
+      /** \brief Sets the output file audio bitrate.
+       *
+       */
+      void setAudioBitrate(const int bitrate);
+
+      /** \brief Returns true if the video and audio codec pair is valid.
+       *
+       */
+      const bool isValid() const;
+
+    private:
+      QString    m_root_directory;    /** last used directory.      */
+      int        m_number_of_threads; /** number of threads to use. */
+      VideoCodec m_videoCodec;        /** output video codec.       */
+      int        m_videoBitrate;      /** output video bitrate.     */
+      AudioCodec m_audioCodec;        /** output audio codec.       */
+      int        m_audioBitrate;      /** output audio bitrate.     */
+
+      /** settings key strings. */
+      static const QString ROOT_DIRECTORY;
+      static const QString NUMBER_OF_THREADS;
+      static const QString VIDEO_CODEC;
+      static const QString VIDEO_BITRATE;
+      static const QString AUDIO_CODEC;
+      static const QString AUDIO_BITRATE;
+  };
+}
+
+#endif // UTILS_H_
